60326493dd0a5de5df7078d6456c9c01c5baa17d
ahawk166 <hawkinsamber16@gmail.com>
2023-02-26 13:36:08 -0600

    Modifying Javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-   }
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*if (topic === 'HTML') {
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+
+function selectTopic(){
+if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
   console.log("Let's study CSS!");
@@ -13,7 +18,10 @@ for (var x = 0; x < topics.length; x++) {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-}*/
+}
+}
 
-var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
